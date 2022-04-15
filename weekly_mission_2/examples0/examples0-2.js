df30e1bd4a2ff3e0d413d2f17351bfb5a2f33827
Arthur-R2D2 <luisv4186@gmail.com>
2022-04-15 14:00:36 -0500

    examples0

---
+++ weekly_mission_2/examples0/examples0-2.js
@@ -0,0 +1,7 @@
+// Ejemplo 2: Crear un objeto con propiedades
+const myObjetc2 = {
+    name: "R2-D2",
+    age: 27
+  }
+  console.log("Ejemplo 2: Crear un objeto con propiedades")
+  console.log(myObjetc2)
---
+++ weekly_mission_2/examples0/examples0-3.js
@@ -0,0 +1,19 @@
+// Ejemplo 3: Objeto con diferentes propiedades
+const myObject3 = {
+    name: "Kasumi",
+    age: 2,
+    nicknames: [
+     "Kashumi",
+      "Kasu",
+      "Kas"
+    ],
+    address: {
+      zip_code: "09360",
+      street: "Iztapalapa",
+      city: "CDMX"
+    }
+  }
+  console.log("Ejemplo 3: Objeto con diferentes propiedades")
+  console.log(myObject3)
+  console.log(myObject3.name)
+  console.log(myObject3["address"])
